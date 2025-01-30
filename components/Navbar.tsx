7cadd7ce4e5259eef19e8c45885d0fd1f6297d5b
AlexBoiko412 <bojkosasa78@gmail.com>
2025-01-30 15:07:05 +0100

    Set font and added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className={"px-14 py-4 navbar-shadow"}>
+            <div className={"flex justify-between items-center"}>
+                <div className="logo">
+                    <img
+                        width={80}
+                        src="https://t3.ftcdn.net/jpg/03/24/30/24/360_F_324302474_PPtpdsBgyhJjxCaWXDwrOzdlYRlPmIh2.jpg"
+                        alt="logo"
+                    />
+                </div>
+                <div className={"flex justify-between items-center gap-6"}>
+                    <div>PROGRAM</div>
+                    <div>
+                        Log In
+                    </div>
+                    <div>
+                        Sign Up
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar

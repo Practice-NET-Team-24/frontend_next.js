faad3f563b402175f067a94ab21f9c30cad40692
AlexBoiko412 <bojkosasa78@gmail.com>
2025-01-31 20:19:12 +0100

    Enhancement

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Home = () => {
     return (
         <div>
-            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/admin">Admin</Link>
         </div>
     );
 };

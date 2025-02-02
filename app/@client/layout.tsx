4e5e2318088ed1bd45584e1a87d99446681acd9e
AlexBoiko412 <bojkosasa78@gmail.com>
2025-02-02 09:11:09 +0100

    Admin Mode Toggle

--- app/@client/layout.tsx
+++ app/@client/layout.tsx
@@ -1,6 +1,7 @@
 
 import type { Metadata } from "next";
 import "../globals.css";
+import React from 'react'
 
 
 import Navbar from "@/components/Navbar";
--- components/ui/modeToggle.tsx
+++ components/ui/modeToggle.tsx
@@ -19,11 +19,13 @@ export function ModeToggle() {
         <DropdownMenu>
             <Toggle asChild>
                 <Button className={"transition-all "} variant="outline" size="icon" onClick={() => setTheme(prev => prev === "light" ? "dark" : "light")}>
-                    {
-                        theme === "dark"
-                            ? <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90" />
-                            : <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+
+                    {theme === "dark"
+                        ? <Sun className={`h-[1.2rem] w-[1.2rem] rotate-0 transition-all dark:-rotate-90 ${theme === "dark" ? "scale-100" : "scale-0"}`} />
+                        :<Moon className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 hover:color-black transition-all dark:rotate-0 ${theme === "dark" ? "scale-0" : "scale-100"}`} />
+
                     }
+
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </Toggle>
